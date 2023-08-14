71c3ca5950b45765118276f8d9953216369000b9
Michael Kohn <mike@mikekohn.net>
2023-08-14 17:55:01 -0500

    Cleanup macros in file_io.h.

--- src/file_io.h
+++ src/file_io.h
@@ -20,26 +20,45 @@
 
 #define ELF_ULONG uint64_t
 
-#define GET_BIG_ENDIAN16(a,b) (a[b]<<8)|(a[b+1]);
-#define GET_BIG_ENDIAN32(a,b) (a[b]<<24)|(a[b+1]<<16)|(a[b+2]<<8)|(a[b+3]);
-#define GET_BIG_ENDIAN64(a,b) ((ELF_ULONG)a[b]<<56)| \
-                              ((ELF_ULONG)a[b+1]<<48)| \
-                              ((ELF_ULONG)a[b+2]<<40)| \
-                              ((ELF_ULONG)a[b+3]<<32)| \
-                              ((ELF_ULONG)a[b+4]<<24)| \
-                              ((ELF_ULONG)a[b+5]<<16)| \
-                              ((ELF_ULONG)a[b+6]<<8)| \
-                              ((ELF_ULONG)a[b+7])
-#define GET_LITTLE_ENDIAN16(a,b) (a[b+1]<<8)|(a[b]);
-#define GET_LITTLE_ENDIAN32(a,b) (a[b+3]<<24)|(a[b+2]<<16)|(a[b+1]<<8)|(a[b]);
-#define GET_LITTLE_ENDIAN64(a,b) ((ELF_ULONG)a[b+7]<<56)| \
-                                 ((ELF_ULONG)a[b+6]<<48)| \
-                                 ((ELF_ULONG)a[b+5]<<40)| \
-                                 ((ELF_ULONG)a[b+4]<<32)| \
-                                 ((ELF_ULONG)a[b+3]<<24)| \
-                                 ((ELF_ULONG)a[b+2]<<16)| \
-                                 ((ELF_ULONG)a[b+1]<<8)| \
-                                 ((ELF_ULONG)a[b]);
+#define GET_BIG_ENDIAN16(a,b) \
+  (a[b + 0] << 8) | \
+  (a[b + 1]);
+
+#define GET_BIG_ENDIAN32(a,b) \
+  (a[b + 0] << 24) | \
+  (a[b + 1] << 16) | \
+  (a[b + 2] << 8)  | \
+  (a[b + 3]);
+
+#define GET_BIG_ENDIAN64(a,b) \
+  ((ELF_ULONG)a[b + 0] << 56) | \
+  ((ELF_ULONG)a[b + 1] << 48) | \
+  ((ELF_ULONG)a[b + 2] << 40) | \
+  ((ELF_ULONG)a[b + 3] << 32) | \
+  ((ELF_ULONG)a[b + 4] << 24) | \
+  ((ELF_ULONG)a[b + 5] << 16) | \
+  ((ELF_ULONG)a[b + 6] << 8)  | \
+  ((ELF_ULONG)a[b + 7])
+
+#define GET_LITTLE_ENDIAN16(a,b) \
+  (a[b + 1] << 8) | \
+  (a[b + 0]);
+
+#define GET_LITTLE_ENDIAN32(a,b) \
+  (a[b + 3] << 24) | \
+  (a[b + 2] << 16) | \
+  (a[b + 1] << 8)  | \
+  (a[b + 0]);
+
+#define GET_LITTLE_ENDIAN64(a,b) \
+  ((ELF_ULONG)a[b + 7] << 56) | \
+  ((ELF_ULONG)a[b + 6] << 48) | \
+  ((ELF_ULONG)a[b + 5] << 40) | \
+  ((ELF_ULONG)a[b + 4] << 32) | \
+  ((ELF_ULONG)a[b + 3] << 24) | \
+  ((ELF_ULONG)a[b + 2] << 16) | \
+  ((ELF_ULONG)a[b + 1] << 8)  | \
+  ((ELF_ULONG)a[b + 0]);
 
 uint16_t get_int16_be(elf_info_t *elf_info, long offset);
 uint32_t get_int32_be(elf_info_t *elf_info, long offset);
